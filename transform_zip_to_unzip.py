ec925d80c5a160703da0c73a232e54fea026c57e
Álvaro Paiva <alvarofepipa@gmail.com>
2024-02-08 14:32:52 -0300

    feat: Script to extract zip files

---
+++ transform_zip_to_unzip.py
@@ -0,0 +1,18 @@
+import os.path
+from tqdm import tqdm
+import zipfile
+
+path_source = 'data/source/'
+path_target = 'data/unzip/'
+
+zip_filenames = [filename for filename in os.listdir(path_source) if filename.endswith('.zip') ]
+for zip_filename in tqdm(zip_filenames):
+    year = int(zip_filename.replace('.zip', ''))
+    if os.path.exists(f'{path_target}{year}'):
+        continue
+    path_to_zip_file = f'{path_source}{zip_filename}'
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        path_target_for_extraction = path_target
+        if year > 2019:
+            path_target_for_extraction += f'{year}/'
+        zip_ref.extractall(path_target_for_extraction)
